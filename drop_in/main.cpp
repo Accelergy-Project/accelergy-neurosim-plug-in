0c11db7cd73382913d9249429070b72c53461b7a
tanner-andrulis <andrulis@mit.edu>
2022-05-19 15:21:03 -0400

    Fixed SRAM energy bug. Added auto access CMOS width.

--- drop_in/main.cpp
+++ drop_in/main.cpp
@@ -281,8 +281,10 @@ vector<double> GetColumnResistance(MemCell& cell, int nCellsOn, int nRowsConnect
 		columnG *= nRowsConnected; // Multiplied by number of connected rows
     } else if (cell.memCellType == Type::SRAM) {	
         // SRAM: weight value do not affect sense energy --> read energy calculated in subArray.cpp (based on wireRes wireCap etc)
-        double totalWireResistance = (double) (resCellAccess + param->wireResistanceCol);
+        // SRAM: weight value do not affect sense energy --> read energy calculated in subArray.cpp (based on wireRes wireCap etc)
+        double totalWireResistance = (double) (resCellAccess + row * param->wireResistanceCol);
         columnG += (double) 1.0/totalWireResistance;
+		columnG *= nRowsConnected;
     }
 
 	// If we're activating rows sequentially, consider the average case per row
